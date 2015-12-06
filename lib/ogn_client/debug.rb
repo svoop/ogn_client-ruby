9a91e224d2b92e1bd5487b79fc415841864f94ba
Sven Schwyn <schwyn@bitcetera.com>
2015-12-06 11:09:12 +0100

    Add support for condition block to debug

--- lib/ogn_client/debug.rb
+++ lib/ogn_client/debug.rb
@@ -1,8 +1,16 @@
 module OGNClient
   class << self
 
+    # Print a debug message to STDOUT if $DEBUG is true
+    #
+    # If you want to print the message based on a condition, you can pass
+    # it as a block:
+    #
+    #   debug('too young') { age < 16 }
+    #
+    # This block will only be evaluated if necessary.
     def debug(message)
-      puts "DEBUG: #{message}" if $DEBUG
+      puts "DEBUG: #{message}" if $DEBUG && (!block_given? || yield)
     end
 
   end
