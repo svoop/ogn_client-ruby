ec5009a0a9c875f7f478fa39411ed3e3d0a5be96
Sven Schwyn <schwyn@bitcetera.com>
2020-03-16 16:51:56 +0100

    Rename config files and add rake task to build checksum

---
+++ rakefile.rb
@@ -0,0 +1,25 @@
+require 'bundler/gem_tasks'
+
+require 'rake/testtask'
+
+Rake::TestTask.new do |t|
+  t.libs << 'lib'
+  t.test_files = FileList['spec/lib/**/*_spec.rb']
+  t.verbose = true
+end
+
+namespace :build do
+  desc "Build checksums of all gems in pkg directory"
+  task :checksum do
+    require 'digest/sha2'
+    Dir.mkdir('checksum') unless Dir.exist?('checksum')
+    Dir.glob('*.gem', base: 'pkg').each do |gem|
+      checksum = Digest::SHA512.new.hexdigest(File.read("pkg/#{gem}"))
+      File.open("checksum/#{gem}.sha512", 'w') { |f| f.write(checksum) }
+    end
+  end
+end
+
+task :default => :test
+
+Dir.glob('tasks/*.rake').each { |task| load task }
