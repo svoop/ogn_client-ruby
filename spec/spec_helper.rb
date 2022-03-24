4bc0520d1b18112d946a9cdb6ec7be24b7bcfe20
Sven Schwyn <schwyn@bitcetera.com>
2022-03-24 17:59:15 +0100

    Remove no longer used spec matchers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,8 +12,6 @@ Minitest::Sound.failure = Pathname(__dir__).join('sounds', 'failure.mp3').to_s
 require 'minitest/reporters'
 Minitest::Reporters.use! [Minitest::Reporters::SpecReporter.new, Minitest::Sound::Reporter.new]
 
-require 'minitest/matchers'
-
 class MiniTest::Spec
   class << self
     alias_method :context, :describe
